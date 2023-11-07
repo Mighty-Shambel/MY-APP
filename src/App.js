0e692a1283c1dcc48732f70989d20ae5ef4b3919
Mighty-Shambel <mightyshambel797@gmail.com>
2023-11-07 12:52:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import logo     from  "./assets/logo.svg"
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const App = () =>{
+  
+   const[count,setCount]=useState(0)
+
+    function addHandle(){
+        setCount(count+1)
+    }
+    function subHandle(){
+        setCount(count-1)
+    }
+    
+    const [tasks,setTasks]=useState([
+        {id:1,Name:"Eat",isCompleted:false},
+        {id:2,Name: "code",isCompleted:true},
+        {id:3,Name:"Sleep",isCompleted:false}
+    ])
+
+    return(
+        <>
+        <Header/>
+        <h1>We Back Again</h1>
+        <img src={logo}  />
+        <p>{count}</p>
+        <button onClick={addHandle}    className="add">ADD</button>
+        <button onClick={subHandle} className="sub">SUBTRACT</button>
+        
+        <h1>List of Tasks</h1>
+        <ul>
+            {tasks.map((task)=>(
+                    <li key={task.id}>
+                    <span>{task.id}-{task.Name}  </span>
+                    <button className="delete">Delete</button>
+                    </li>
+            ))}
+        </ul>
+        <Footer/>
+        </>
+        
+    )
 }
 
-export default App;
+export default App
